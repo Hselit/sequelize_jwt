067d7b0aa959cdb1c71b4755f200346fdee5084e
Hselit <tileshkathiresan@gmail.com>
2025-03-31 11:32:48 +0530

    implemented jwt

--- sequelize_jwt/routes/users.js
+++ sequelize_jwt/routes/users.js
@@ -1,9 +1,45 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const db = [
+  {
+  name:"Arun",
+  password:"123456"
+  }
+]
+
+router.post('/generate', function(req, res, next) {
+  try{
+    const data = req.body;
+    const{name,password} = data;
+    const user = db.find(id => id.name == name && id.password == password);
+
+    if(user){
+      const jwttoken = jwt.sign(data,process.env.SECRET_KEY,{expiresIn:"1h"});
+      console.log(jwttoken);
+      return res.status(200).json({message:"Success","token":jwttoken});
+    }
+    return res.status(401).json({message:"Invalid Credentials"});
+
+  }
+  catch(err){
+    console.log("Error Occured "+err);
+  }
+});
+
+router.get('/verify',function(req,res){
+  const token = req.headers['authorization'];
+  if(!token){
+    return res.status(403).json({message:"Token Required"});
+  }
+  jwt.verify(token,process.env.SECRET_KEY,(err,decoded)=>{
+    if(err){
+      return res.status(401).json({message:"Invalid Token"});
+    }
+    res.status(200).json({message:"Verified Successfully"});
+  })
 });
 
 module.exports = router;
